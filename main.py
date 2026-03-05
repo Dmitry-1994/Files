3840116da535cceb09dfe972c009b5a1d8c0041f
Dmitry Tarasov <vizalyyy@gmail.com>
2026-03-05 23:20:31 +0500

    Add task 2

--- main.py
+++ main.py
@@ -8,11 +8,8 @@ def readReciepes(filename):
             if not line:
                 continue
             elif not line.isdigit() and not "|" in line:
-                dishName = line.strip()
+                dishName = line
                 cookBook[dishName] = []
-            # elif line.isdigit():
-            #     countIngredient = int(line)
-            #     print(countIngredient)
             elif "|" in line:
                 dictIngredient = parseIngridients(line)
                 cookBook[dishName].append(dictIngredient)
@@ -30,5 +27,19 @@ def parseIngridients(stringIngredient: str):
         'measure': ingredientMeasure
     }
 
-print(readReciepes("recipes.txt"))
+def getShopListByDishes(dishes: list, personCount: int):
+    cookBook = readReciepes("recipes.txt")
+    shopList = {}
+    for dish in dishes:
+        for ingredient in cookBook[dish]:
+            name = ingredient['ingredient_name']
+            quantity = ingredient['quantity'] * personCount
+            measure = ingredient['measure']
+            if name in shopList:
+                shopList[name]['quantity'] += quantity
+            else:
+                shopList[name] = {'measure': measure, 'quantity': quantity}
+    return shopList
+
+print(getShopListByDishes(['Запеченный картофель', 'Омлет'], 2))
 
